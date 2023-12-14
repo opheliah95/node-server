36914174e489f5e472e71f34a47534f1dee7dcb2
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-12-14 17:12:12 +0000

    added basic server function to handle different web pages

--- logEvents.js
+++ logEvents.js
@@ -4,24 +4,25 @@ const fsPromise = require('fs').promises;
 const fs = require('fs')
 const path = require('path')
 
-const logEvent = async (message) => {
+const logEvent = async (message, fileName) => {
     const dateTime = `${format(new Date(), 'MM/dd/yyyy\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}`;
+    const dir = path.join(__dirname, 'logs')
+    console.log(`writing to ${dir}`)
     console.log(logItem);
     try{
         if(!fs.existsSync(dir)) {
-            await fsPromise.mkdirSync(dir, { recursive: true });
+            fs.mkdirSync(dir, { recursive: true });
         }
-        await fsPromise.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), `\n${logItem}`, { encoding: 'utf8' })
+        await fsPromise.appendFile(path.join(dir, fileName), `\n${logItem}`, { encoding: 'utf8' })
     } catch (err){
-        console.log("the error code is now: ", err.code)
+        console.log("the error code is now: ", err)
         if(err.code === 'ENOENT') {
             console.log("building path")
-            const dir = path.join(__dirname, 'logs')
             if(!fs.existsSync(dir)) {
                 fs.mkdirSync(dir, { recursive: true });
             }
-            fs.writeFile(path.join(dir, 'eventLog.txt'), msg,{ encoding: 'utf8' })
+            fs.writeFile(path.join(dir, fileName), msg,{ encoding: 'utf8' })
         }
     }
 }
--- server.js
+++ server.js
@@ -5,21 +5,107 @@ const path = require('path')
 const EventEmitter = require("events");
 const logEvent = require('./logEvents')
 
+// emitter function
 class MyEmitter extends EventEmitter { };
 const myEmitter = new MyEmitter();
+myEmitter.on("log", (msg, fileName) => {
+    logEvent(msg, fileName)
+})
+
 const port = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const rawData = await fsPromises.readFile(filePath, 
+            !contentType.includes("image")? "utf8" : ""
+            );
+        const data = contentType == "application/json" ?
+            JSON.parse(rawData) : rawData;
+
+        response.writeHead(
+            !filePath.includes("404")?  404: 200,
+            {'Content-Type' : contentType}
+            )
+        //console.log(`the data is ${rawData}`)
+        response.end(
+            contentType == 'application/json' ? JSON.stringify(data) : data);
+    } catch (err) {
+        console.log(err);
+        response.statusCode = 500;
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.end();
+    }
+}
+
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+    console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
 
-}).listen(port, () => {
-    console.log(`server running on ${port}`)
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+    console.log(`that is ${req.url.slice(-1)}`);
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+    const fileExists = fs.existsSync(filePath);
+    console.log(`the filepath is ${filePath}`)
+
+    if (fileExists) {
+        serveFile(filePath, contentType, res);
+    } else {
+        console.log("base for erro file: ", path.parse(filePath).base)
+        switch (path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, {
+                    'Location': '/new-page.html'
+                })
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, {
+                    'Location': '/',
+                    'Content-Type': 'text/html'
+                })
+                res.end();
+                break;
+            default:
+                serveFile(path.join(__dirname, "views", "404.html"), contentType, res)
+                break;
+        }
+
+
+    }
 });
-
-// myEmitter.on('log', (msg) => {
-//     logEvent(msg);
-// })
-
-// setTimeout(() => {
-//     myEmitter.emit('log', 'log event emitted')
-// }, 60)
+server.listen(port, () => console.log(`Server running on port ${port}`));
